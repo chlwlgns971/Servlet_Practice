b9c72971231618975218d32c671d6d3cc5850764
Jihoon <Jihoon@DESKTOP-RDP9RGT>
2022-09-02 19:32:25 +0900

    2022.09.01 파일 Stream 학습 및 연습

---
+++ LanguageLab/src/main/java/kr/or/ddit/io/StreamDesc.java
@@ -0,0 +1,142 @@
+package kr.or.ddit.io;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * Stream 이용한 IO
+ * 
+ * Stream : 연속성을 지닌 일련의 데이터 집합 이면서 동시에 데이터를 읽거나 쓰기 위한
+ * 			단방향(***) 통로
+ * 
+ * 	스트림을 사용한 IO 단계
+ * 	1. media(매체)를 어플리케이션에서 제어할 수 있는 객체의 형태로 생성.
+ * 		ex) new File(filesystem Path), Socket, memory
+ * 	2. 1차 스트림을 매체에 연결.
+ * 		ex) new FileInputStream(file)
+ * 	3. 2차 스트림을 1차 스트림에 연결. (optional)
+ * 		ex) new BufferedInputStream(inputStream)
+ * 	4. EOF(EOS, -1, null) 까지 반복적인 read
+ * 	5. close
+ * 
+ *  stream 종류
+ *  1. 전송 데이터 크기
+ *  	1) byte stream	(~~Stream)
+ *  		FileInputStream / FileOutputStream
+ *  		SoketInputStream / SoketOutputStream
+ *  		ByteArrayInputStream / ByteArrayOutputStream 
+ *  	2) character stream (~~Reader/Writer)
+ *  		FileReader / FileWriter
+ *  		
+ *  2. stream 생성 방법에 따라.
+ *  	1) 1차 stream
+ *  	2) 2차 stream(연결형 스트림)
+ *  		BufferedReader (readLine)
+ *  		FilteredStream : DataInputStream
+ *  		ObjectInput[Output]Stream : Serializable 객체 직렬화/역지렬화 스트림.
+ * 
+ */
+public class StreamDesc {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		//readAndPrintFileSystemResource();
+		readAndPrintClassPathResource();
+		//readAndWriteToFileSystemResource();
+	}
+	private static void readAndWriteToFileSystemResource() throws URISyntaxException, IOException{
+		String logo = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+		String filePath = "D:\\contents\\googleLogo.png";
+		
+		
+		URI logoURI = new URI(logo);
+		URL logoURL = logoURI.toURL();
+		File file = new File(filePath);
+		
+		try(
+			InputStream is = logoURL.openStream();
+			FileOutputStream os = new FileOutputStream(file);
+			
+			BufferedInputStream bis = new BufferedInputStream(is);
+			BufferedOutputStream bos = new BufferedOutputStream(os);
+		){
+			int eof = -1;
+			while ((eof=bis.read()) != -1) {
+				bos.write(eof);
+			}
+		}
+		
+		
+	}
+	
+	public static void  readAndPrintClassPathResource() throws URISyntaxException, IOException{
+		//String resCpPath = "/kr/or/ddit/medias/오래된 노래_utf8.txt";
+		String resCpPath = "/kr/or/ddit/medias/오래된 노래.txt";
+		String filePath = "D:\\contents\\오래된 노래.txt";
+		try(
+			// step2
+			InputStream is = StreamDesc.class.getResourceAsStream(resCpPath);
+			InputStreamReader isr = new InputStreamReader(is, "MS949");
+				
+			OutputStream os = new FileOutputStream(filePath);
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			// step3
+			BufferedReader br = new BufferedReader(isr);
+			BufferedWriter bw = new BufferedWriter(osw);
+		){
+			String temp = null;
+			// step4
+			while ((temp =br.readLine()) != null) {
+				System.out.println(temp);
+				bw.write(temp+"\n");
+			}
+		}
+			// step5 try-with- resource 사용 생략 
+	}
+
+	private static void readAndPrintFileSystemResource() throws IOException {
+		String resPath = "D:\\contents\\another day.txt";
+
+//		try with resource 문법 
+//		try (
+//				Closable 객체 생성. -> 자동으로 finally 가 추가되고, close됨.
+//		){
+//			
+//		}catch (IOException e) {
+//		}
+		
+		// step1
+		File file = new File(resPath);
+		try(		
+			// step2
+			FileReader reader = new FileReader(file);
+			// step3
+			BufferedReader br = new BufferedReader(reader);
+		){
+			String temp = null;
+			// step4
+			while ( (temp = br.readLine()) != null ) {
+				System.out.println(temp);
+			}
+		}
+		
+		// step5
+//		br.close();
+//		reader.close();
+		
+	}
+}
